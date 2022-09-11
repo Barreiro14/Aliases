050147d94ead0cd906ae5ae15fd2734bd3444bc9
Barry <g.barreiro140@gmail.com>
2022-09-11 13:01:35 -0400

    add FORTRAN scritp

---
+++ tFortran.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+touch tarea${1}GabrielBarreiro.f90
+echo "!Gabriel Barreiro 100427354" > tarea${1}GabrielBarreiro.f90
+echo "!Licenciatura en fisica" >> tarea${1}GabrielBarreiro.f90
+echo "!Tarea ${1} INF 2120-04" >> tarea${1}GabrielBarreiro.f90
+echo "!Universidad Autonoma de Santo Domingo" >> tarea${1}GabrielBarreiro.f90
+echo "!segundo semestre del 2022" >> tarea${1}GabrielBarreiro.f90
+echo "program Tarea${1}GabrielBarreiro" >> tarea${1}GabrielBarreiro.f90
+echo "  " >> tarea${1}GabrielBarreiro.f90
+echo "end program Tarea${1}GabrielBarreiro" >> tarea${1}GabrielBarreiro.f90
+
+touch runTest.sh
+echo "gfortran -o tarea${1} tarea${1}GabrielBarreiro.f90" > runTest.sh
+echo "./tarea${1}" >> runTest.sh
